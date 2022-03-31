9e5880b41d1f5954ee3114422de0046150f743bc
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-03-31 17:08:29 -0500

    add tls

---
+++ security-6/server.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/secret', (req, res) => {
+  return res.send('Your personal secret value is 42!!')
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+https.createServer({
+  cert: fs.readFileSync('cert.pem'),
+  key: fs.readFileSync('key.pem'),
+}, app).listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
