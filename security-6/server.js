c73801177e851ca75f79289a8f68c70b57aa437b
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-04-03 16:24:07 -0500

    Add passport middleware

--- security-6/server.js
+++ security-6/server.js
@@ -1,17 +1,57 @@
+require('dotenv').config();
+
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
 const express = require('express');
 const helmet = require('helmet');
+const passport = require('passport');
+const Strategy = require('passport-google-oauth20');
 
 const PORT = 3000;
+
+const config = {
+  CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  CLIENT_SECRET: process.env.GOOGLE_SECRET,
+};
+
+const AUTH_OPTIONS = {
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+  callbackURL: '/auth/google/callback',
+};
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
 
 const app = express();
 
 app.use(helmet());
+app.use(passport.initialize());
 
-app.get('/secret', (req, res) => {
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['email']
+  })
+);
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session: false,
+  })
+);
+
+app.get('/auth/logout', (req, res) => {
+
+});
+
+app.get('/secret', checkLoggedIn, (req, res) => {
   return res.send('Your personal secret value is 42!!')
+});
+
+app.get('/failure', (req, res) => {
+  return res.send('Failed to log in!');
 })
 
 app.get('/', (req, res) => {
@@ -24,3 +64,20 @@ https.createServer({
 }, app).listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true;
+
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: 'You must log in',
+    });
+  }
+
+  next();
+}
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log('Google profile', profile);
+  done(null, profile);
+}
