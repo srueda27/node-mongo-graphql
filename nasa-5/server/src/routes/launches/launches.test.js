bff4464488768325d544cf9b3a93b353d356a188
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-04-04 08:51:46 -0500

    Load planets data in test

--- nasa-5/server/src/routes/launches/launches.test.js
+++ nasa-5/server/src/routes/launches/launches.test.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const request = require('supertest');
 const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
+const { loadPlanetsData } = require('./src/models/planets.model');
 
 describe('Launches API', () => {
   beforeAll( async () => {
     await mongoConnect();
+    await loadPlanetsData();
   });
 
   afterAll( async () => {
