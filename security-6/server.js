b1b22d512ca2c975d237303a64bf99a6eacfee53
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-04-03 22:29:32 -0500

    Add session cookie and logout

--- security-6/server.js
+++ security-6/server.js
@@ -7,12 +7,15 @@ const express = require('express');
 const helmet = require('helmet');
 const passport = require('passport');
 const Strategy = require('passport-google-oauth20');
+const cookieSession = require('cookie-session');
 
 const PORT = 3000;
 
 const config = {
   CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   CLIENT_SECRET: process.env.GOOGLE_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 const AUTH_OPTIONS = {
@@ -21,13 +24,31 @@ const AUTH_OPTIONS = {
   callbackURL: '/auth/google/callback',
 };
 
-passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+passport.serializeUser((user, done) => {
+  //this user is the profile object that comes from the verifyCallback function
+  done(null, user.id);  
+});
+
+passport.deserializeUser((id, done) => {
+  //This is where & how the req.user is populated from the cookies in the middleware
+  done(null, id);
+})
 
 const app = express();
 
+//middlewares
 app.use(helmet());
+app.use(cookieSession({
+  name: 'session',
+  maxAge: 1000 * 60 * 60 * 24,
+  keys: [ config.COOKIE_KEY_1, config.COOKIE_KEY_2 ],
+}))
 app.use(passport.initialize());
+app.use(passport.session());
 
+//endpoints
 app.get('/auth/google',
   passport.authenticate('google', {
     scope: ['email']
@@ -38,12 +59,12 @@ app.get('/auth/google/callback',
   passport.authenticate('google', {
     failureRedirect: '/failure',
     successRedirect: '/',
-    session: false,
   })
 );
 
 app.get('/auth/logout', (req, res) => {
-
+  req.logOut(); //Removes req.user and clears any logged in session
+  return res.redirect('/');
 });
 
 app.get('/secret', checkLoggedIn, (req, res) => {
@@ -66,7 +87,9 @@ https.createServer({
 });
 
 function checkLoggedIn(req, res, next) {
-  const isLoggedIn = true;
+  //comes from the deserializeUser middleware function (with the done callback function)
+  console.log('Current user: ', req.user)
+  const isLoggedIn = req.isAuthenticated() && req.user;
 
   if (!isLoggedIn) {
     return res.status(401).json({
@@ -78,6 +101,5 @@ function checkLoggedIn(req, res, next) {
 }
 
 function verifyCallback(accessToken, refreshToken, profile, done) {
-  console.log('Google profile', profile);
   done(null, profile);
 }
